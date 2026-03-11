799d42faf8a571717b2fc5e1d4e297275a8b10f3
gapinsky <gapinski02@gmail.com>
2026-03-11 21:26:01 +0100

    pricing togglebuttons active styles

--- myComponents/pages/pricing/ToggleButtons.tsx
+++ myComponents/pages/pricing/ToggleButtons.tsx
@@ -33,10 +33,10 @@ export default function TabsNav() {
           <Link
             key={tab.segment}
             href={tab.href}
-            className={`rounded-full px-6 py-3 transition ${
+            className={`whitespace-nowrap uppercase rounded-full border-2   text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive  text-slate-50  h-9 px-4 py-2 has-[>svg]:px-3 w-fit  ${
               isActive
-                ? "bg-red-500 text-black"
-                : "bg-neutral-200 text-black hover:bg-neutral-300"
+                ? "bg-(--brand-700) border-transparent hover:bg-(--brand-900)"
+                : "bg-none border-2 hover:border-transparent hover:bg-black/5 dark:hover:bg-white/10"
             }`}
           >
             {tab.label}
