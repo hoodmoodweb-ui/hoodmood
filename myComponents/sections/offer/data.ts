1c3f4621d1d754131b68e9ca4f5a3ec0ba8ea0b8
gapinsky <gapinski02@gmail.com>
2026-03-27 12:38:52 +0100

    hero > offer section descriptions

--- myComponents/sections/offer/data.ts
+++ myComponents/sections/offer/data.ts
@@ -9,28 +9,27 @@ export const offer = [
   {
     title: "Dzieci",
     description:
-      "Akrobatyka, Taniec Towarzyski, Street Dance, Video Clip, Cheerleading",
+      "Balet, Taniec, Breakdance, Akrobatyka, Formacje taneczne, Zajęcia indywidualne, Partnerowanie akrobatyczne ",
     img: "/assets/images/landingOffer/kids1.jpg",
     hoverImg: "/assets/images/landingOffer/kids2.jpg",
   },
   {
     title: "Młodzież",
     description:
-      "Akrobatyka, Taniec Towarzyski, Street Dance, Video Clip, Cheerleading",
+      "Akrobatyka, Formacje taneczne, Taniec współczesny, Hip-Hop, Zajęcia indywidualne, K-POP, Partnerowanie akrobatyczne",
     img: "/assets/images/landingOffer/teens1.jpg",
     hoverImg: "/assets/images/landingOffer/teens2.jpg",
   },
   {
     title: "Dorośli",
-    description:
-      "Akrobatyka, Taniec Towarzyski, Street Dance, Video Clip, Cheerleading",
+    description: "Feminine Flow, Formacje taneczne, Zajęcia indywidualne",
     img: "/assets/images/landingOffer/adults2.jpg",
     hoverImg: "/assets/images/landingOffer/adults1.jpg",
   },
   {
     title: "Obozy, kolonie, pokazy",
     description:
-      "Akrobatyka, Taniec Towarzyski, Street Dance, Video Clip, Cheerleading",
+      "Półkolonie, kolonie, pokazy oraz aktywny czas pełen tańca, sportu, integracji i dodatkowych atrakcji, takich jak paintball czy park linowy",
     img: "/assets/images/landingOffer/camp1.jpg",
     hoverImg: "/assets/images/landingOffer/camp2.jpg",
   },
